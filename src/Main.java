389fdc13eac73cdff0c79a785bd282bfe8704f1f
Matthieu Eric Moundou <38407717+mmoundou@users.noreply.github.com>
2022-06-13 03:50:29 -0400

    Solution to the Fizz Buzz test

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int index = 1; index <= 100; index++) {
+
+            if(index % 3 == 0 && index % 5 == 0)
+                System.out.println("fizzbuzz");
+            else if(index % 5 == 0)
+                System.out.println("buzz");
+            else if(index % 3 == 0)
+                System.out.println("fizz");
+            else
+                System.out.println(index);
+
+        }
+    }
+}
