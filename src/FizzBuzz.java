4ab3b3dd7a49f93e5bbaa122f6a90d5ac86e1607
Matthieu Eric Moundou <38407717+mmoundou@users.noreply.github.com>
2022-06-13 03:57:37 -0400

    Nested Fizz Buzz solution within a separate method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,15 +1,19 @@
 public class FizzBuzz {
 
-    for(int index = 1; index <= 100; index++) {
+    public void displayFizz() {
 
-        if(index % 3 == 0 && index % 5 == 0)
-            System.out.println("fizzbuzz");
-        else if(index % 5 == 0)
-            System.out.println("buzz");
-        else if(index % 3 == 0)
-            System.out.println("fizz");
-        else
-            System.out.println(index);
+        for (int index = 1; index <= 100; index++) {
+
+            if (index % 3 == 0 && index % 5 == 0)
+                System.out.println("fizzbuzz");
+            else if (index % 5 == 0)
+                System.out.println("buzz");
+            else if (index % 3 == 0)
+                System.out.println("fizz");
+            else
+                System.out.println(index);
+
+        }
 
     }
 
